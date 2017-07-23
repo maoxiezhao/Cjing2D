388441bbb652f8eca27ce775bb61102726ce8bd0
zhaoyang <844635471@qq.com>
2017-07-23 18:34:20 +0800

    map基本实现

--- src/game/map.cpp
+++ src/game/map.cpp
@@ -1,5 +1,56 @@
 #include "map.h"
+#include "core\inputEvent.h"
+#include "game\game.h"
+#include "lua\luaContext.h"
 
-Map::Map()
+Map::Map(const std::string& id):
+	mMapID(id),
+	mGame(nullptr),
+	mWidth(0),
+	mHeight(0),
+	mMinLayer(0),
+	mMaxLayer(0),
+	mIsLoaded(false),
+	mIsStarted(false),
+	mCamera(nullptr),
+	mTileset(nullptr),
+	mEntities(nullptr)
 {
+}
+
+void Map::Update()
+{
+	CheckSuspended();
+
+	DrawBackground();
+	
+	mEntities->Update();
+	GetLuaContext().OnMapUpdate();
+
+	DrawForeground();
+}
+
+void Map::CheckSuspended()
+{
+	bool gameSuspended = mGame->IsSuspended();
+	if (mSuspended !=  gameSuspended)
+	{
+		SetSuspended(gameSuspended);
+	}
+}
+
+void Map::SetSuspended(bool suspended)
+{
+	mSuspended = suspended;
+}
+
+void Map::Draw()
+{
+	if (!IsLoaded())
+	{
+		return;
+	}
+
+	mEntities->Draw();
+	GetLuaContext().OnMapDraw();
 }

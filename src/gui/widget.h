affa9fc4ea923fc66399a200bca7affa4a83b526
zhaoyang <844635471@qq.com>
2017-07-31 20:35:49 +0800

    widget base class

---
+++ src/gui/widget.h
@@ -0,0 +1,80 @@
+#pragma once
+
+#include"common\common.h"
+#include"game\sprite.h"
+#include"utils\point.h"
+
+/**
+*	\brief widget»ùÀà
+*/
+class Widget
+{
+	/*** *** *** *** *** property. *** *** *** *** ***/
+public:
+	enum class Visiblility
+	{
+		Visible,
+		Hidden,
+		InVisible
+	};
+
+	enum class ReDrawAction
+	{
+		Full,
+		None
+	};
+
+public:
+	Widget();
+	virtual ~Widget();
+
+	Widget(const Widget&) = delete;
+	Widget& operator=(const Widget&) = delete;
+
+	void SetID(const string& id);
+	const string& GetID()const;
+
+	void DrawBackground();
+	void DrawForeground();
+	void DrawChildren();
+
+	void SetIsDirty(bool isDirty);
+	bool IsDirty()const;
+	const Rect GetDirtyRect()const;
+
+	Visiblility GetVisibility()const;
+	void SetVisibility(const Visiblility& visibility);
+
+private:
+	string mID;		
+
+	std::weak_ptr<Widget> mParent;	
+
+	bool mIsDirty;
+
+	Visiblility mVisible;
+
+	/*** *** *** *** *** layout and size. *** *** *** *** ***/
+public:
+	const Point2& GetPositon()const;
+	void SetPosition(const Point2& position);
+	const Size& GetSize()const;
+	void SetSize(const Size& size);
+	int GetWidth()const;
+	int GetHeight()const;
+
+	virtual void Move(const Point2& offset);
+	virtual void Move(const int xoffset, const int yoffset);
+
+	virtual void InitLayout();
+	virtual Size GetBestSize()const;
+
+	virtual void SetHorizontalAlignment();
+	virtual void SetVerticalAlignment();
+
+private:
+	Point2 mPosition;
+
+	Size mSize;
+
+};

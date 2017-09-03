59e7f86de0ce6818ba3e7dfc219d220e6cd59b56
maoxiezhao <844635471@qq.com>
2017-09-03 18:12:42 +0800

    补充部分data资源；脚本中添加异步加载场景；

---
+++ data/menus/loading.lua
@@ -0,0 +1,48 @@
+local LoadingScene = {};
+
+-- loading menu
+-- 用于加载字体以及其他游戏资源
+
+function LoadingScene:onStarted()
+	print "LoadingScene Starting.";
+	self._asyncLoader = nil
+	self._animation = nil
+	
+	local animation = cjing.Animation.create("menus/loading")
+	if animation then 
+		animation:setPos(260, 180)
+		animation:setSize(120,120)
+		animation:start()
+		self._animation = animation
+	end
+	
+	local asyncLoader = cjing.AsyncLoader.create()
+	if asyncLoader then 
+		self._asyncLoader = asyncLoader
+		asyncLoader:addTask(self.loadingFunction)
+		asyncLoader:setFinishedCallBack(self.loadingFinishedCallBack)
+		asyncLoader:run()
+	end 
+	
+end
+
+function LoadingScene:loadingFunction()
+	print "Loading...."
+	
+	-- 加载默认字体
+	cjing.Text.loadDefault()
+	
+end
+
+function LoadingScene:loadingFinishedCallBack()
+	print "Loading Finished"
+	cjing.Menu.Stop(LoadingScene)
+end
+
+function LoadingScene:onDraw()
+	if self._animation then 
+		self._animation:draw()
+	end 
+end
+
+return LoadingScene;

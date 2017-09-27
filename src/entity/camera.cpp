5d7268ce092ed571563fc3eed9f86e35c662d9e1
maoxiezhao <844635471@qq.com>
2017-09-27 22:11:13 +0800

    添加简单相机，支持entity定位

--- src/entity/camera.cpp
+++ src/entity/camera.cpp
@@ -1,11 +1,50 @@
 #include"camera.h"
+#include"game\map.h"
+#include"core\video.h"
+#include"entity\entityState.h"
 
-Camera::Camera()
+namespace {
+	/**
+	*	\brief 相机跟踪状态
+	*
+	*	该状态使当前相机的相机位置始终设定为跟踪的entity
+	*/
+	class TracingState : public EntityState
+	{
+	public:
+		TracingState(Camera& camera, Entity& tracingEntity);
+
+		virtual void Update();
+	private:
+		Entity& mTracingEntity;
+	};
+
+	TracingState::TracingState(Camera& camera, Entity & tracingEntity):
+		EntityState(camera),
+		mTracingEntity(tracingEntity)
+	{
+	}
+
+	void TracingState::Update()
+	{
+		Camera& camera = *dynamic_cast<Camera*>(&GetEntity());
+		Point2 centerPos = mTracingEntity.GetCenterPos();
+		camera.SetPos(centerPos);
+	}
+
+}
+
+Camera::Camera(Map& map):
+	Entity("", Point2(0, 0), Video::GetScreenSize(), 0)
 {
+	SetMap(&map);
 }
 
 void Camera::Update()
 {
+	Entity::Update();
+
+	AdaptToMapBound();
 }
 
 void Camera::Draw()
@@ -20,4 +59,64 @@ EntityType Camera::GetEntityType() const
 const string Camera::GetLuaObjectName() const
 {
 	return string();
+}
+
+/**
+*	\brief 获取总的视野大小
+*/
+Size Camera::GetViewSize() const
+{
+	return GetSize() * 2;
+}
+
+/**
+*	\brief 获取左上角坐标
+*/
+Point2 Camera::GetLeftTopPos() const
+{
+	return Point2(GetPos().x - GetSize().width,
+		GetPos().y - GetSize().height);
+}
+
+/**
+*	\brief 设置左上角坐标
+*/
+void Camera::SetLeftTopPos(const Point2 & pos)
+{
+	SetPos({ pos.x + GetSize().width,
+		pos.y + GetSize().height });
+}
+
+/**
+*	\brief 调整camera的大小适应地图大小
+*/
+void Camera::AdaptToMapBound()
+{
+	Size mapSize = GetMap().GetSize();
+	Size viewSize = GetViewSize();
+	Point2 newCamearPos;
+
+	// 处理适配各个坐标方向
+	auto adaptSizeAxis = [](int viewSize, int mapSize, int camPos) ->int
+	{
+		if (mapSize < viewSize)
+		{
+			return (mapSize - viewSize) / 2;
+		}
+		else
+		{
+			return std::min(std::max(camPos, 0), (mapSize - viewSize));
+		}
+	};
+	newCamearPos.x = adaptSizeAxis(viewSize.width,  mapSize.width,  GetLeftTopPos().x);
+	newCamearPos.y = adaptSizeAxis(viewSize.height, mapSize.height, GetLeftTopPos().y);
+	SetLeftTopPos(newCamearPos);
+}
+
+/**
+*	\brief 设置相机为追踪entity移动
+*/
+void Camera::TracingEntity(Entity & entity)
+{
+	SetState(std::make_shared<TracingState>(*this, entity));
 }
--- src/entity/camera.h
+++ src/entity/camera.h
@@ -8,19 +8,26 @@
 *	\brief 相机对象
 *
 *	相机对象主要是用来对地图限制位置进行控制，同时提供总体的显示特效
+*	其中相机的entity属性中：pos指的是相机的中心坐标，size为相机xy方向
+*	上的范围，总的范围应为2倍的size
 */
 class Camera : public Entity
 {
 public:
-	Camera();
+	Camera(Map& map);
 
 	virtual void Update();
 	virtual void Draw();
 
 	virtual EntityType GetEntityType()const;
 	virtual const string GetLuaObjectName()const;
-private:
 
+	Size GetViewSize()const;
+	Point2 GetLeftTopPos()const;
+	void SetLeftTopPos(const Point2& pos);
+	void AdaptToMapBound();
+
+	void TracingEntity(Entity& entity);
 
 };
 

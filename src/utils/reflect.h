de756394ede4ddad080be1277f9e7b94412a1cd0
maoxiezhao <844635471@qq.com>
2017-11-08 22:45:22 +0800

    完善reflect，但仍存在很大局限，函数的参数类型目前不支持指针和引用类型

--- src/utils/reflect.h
+++ src/utils/reflect.h
@@ -5,6 +5,8 @@
 #include<typeinfo>
 #include<functional>
 #include<vector>
+#include<type_traits>
+#include<memory>
 
 #include"core\debug.h"
 
@@ -17,9 +19,7 @@
 
 namespace util
 {
-namespace Refl 
-{
-
+	namespace refel{
 	class Instance;
 
 	/**
@@ -59,11 +59,27 @@ namespace Refl
 		}
 
 		/**
-		*	\brief 调用指定函数
+		*	\brief 执行指定函数
 		*	\param args 该函数的参数，应已经包装为instance
+		*/
+		Instance Apply(Instance* args[], size_t argc, const std::string& funcName);
+
+		/**
+		*	\brief 调用指定函数
+		*
+		*	该函数处理参数并传递给apply
 		*/
 		template<typename... Args>
 		Instance Call(const std::string& funcName, Args&&... args);
+
+		/**
+		*	\brief 调用指定函数
+		*
+		*	该函数处理参数并传递给apply,保证传入的参数全部
+		*	已经instance
+		*/
+		template<typename... Args>
+		Instance CallByInstances(const std::string& funcName, Args&&... args);
 
 		// 获取函数类型
 		FuncType& GetFunction(const std::string& funcName)
@@ -97,8 +113,11 @@ namespace Refl
 		/**
 		*	\brief 注册成员函数
 		*/
-		/*	template<typename T, typename ReturnType, typename... Args>
-		void Function(const std::string& funcName, ReturnType(T::*f)(Args...));*/
+		template<typename T, typename ReturnType, typename... Args>
+		void Function(const std::string& funcName, ReturnType(T::*f)(Args...));
+		template<typename T, typename... Args>
+		void Function(const std::string& funcName, void(T::*f)(Args...));
+
 	private:
 		std::string mName;
 		const type_info& mTypeInfo;
@@ -153,11 +172,16 @@ namespace Refl
 		template<typename... Args>
 		void EnableConstruct()
 		{
-			/*		Function( std::string("_Init_") + std::to_string(sizeof...(Args)),
-			[&](Args&&
-			);*/
+			Function(std::string("_Init_") + std::to_string(sizeof...(Args)),
+				Init<Args...>);
 		}
 
+	private:
+		template<typename... Args>
+		static T Init(Args... args)
+		{
+			return T(std::forward<Args>(args)...);
+		}
 	};
 
 
@@ -178,6 +202,7 @@ namespace Refl
 			RegisterClass<double>("double");
 			RegisterClass<bool>("bool");
 			RegisterClass<std::string>("string");
+			RegisterClass<void*>("pointer");
 		}
 
 		template<typename T>
@@ -239,7 +264,7 @@ namespace Refl
 		std::map<std::string, size_t> mClassNames;	// 类名和type_info hascode的映射表
 	};
 
-
+	///////////////////////////////////////////////////////////////////
 	/**
 	*	\brief 存储数据真实值
 	*/
@@ -269,52 +294,121 @@ namespace Refl
 	class Instance
 	{
 	public:
-		template<typename T>
-		static Instance Make(T o)
+		using InstancePtr = std::unique_ptr<Instance>;
+		// 创建非指针数据instance
+		template<typename T,
+			typename Enable = typename std::enable_if<!std::is_pointer<T>::value>::type >
+			static Instance Make(T o)
 		{
 			Instance i;
 			i.mMeta = &MetaManager::GetInstance().GetMeta<T>();
-			i.mValue.reset(new TypeInstanceValue<T>(std::move(o)));
+			i.mValue.reset(new TypeInstanceValue<
+				typename std::remove_reference<T>::type>(std::move(o)));
 
 			return std::move(i);
 		}
 
-		Instance() :mMeta(nullptr) {}
+		// 创建指针数据instance,mMeta为void*
+		template<typename T>
+		static Instance Make(const T* p)
+		{
+			Instance i = Instance::Make<void*, void>(const_cast<T*>(p));
+			i.mPointerMeta = &MetaManager::GetInstance().GetMeta<T>();
+			return std::move(i);
+		}
+
+
+		Instance() : mMeta(nullptr), mPointerMeta(nullptr), mValue(nullptr) {}
 		Instance(Instance&& i) :
-			mMeta(i.mMeta), mValue(std::move(i.mValue)) {}
+			mMeta(i.mMeta), mPointerMeta(i.mPointerMeta), mValue(std::move(i.mValue)) {}
 		Instance(const Instance& i) = delete;
 
 		Meta* GetMeta() { return mMeta; }
+		Meta* GetPointerMeta() { return mPointerMeta; }
+		bool IsPointer()const { return mMeta->IsSame<void*>(); }
 
+		/**
+		*	\brief 获取当前instance值
+		*/
 		template<typename T>
 		T& GetValue()
 		{
+			// 非指针
 			if (mMeta->IsSame<T>())
 			{
 				auto p = dynamic_cast<TypeInstanceValue<T>*>(mValue.get());
 				return p->mData;
 			}
+
+			// 指针数据，且数据可能
+			if (!IsPointer())
+			{
+				Debug::Error("Unable to get the value");
+			}
+			auto p = dynamic_cast<TypeInstanceValue<void*>*>(mValue.get());
+			return *static_cast<T*>(p->mData);
 		}
 
 	private:
-		Meta* mMeta;
+		Meta* mMeta;	    // 类型信息，如果是指针则为void*
+		Meta* mPointerMeta;	// 指针类型信息
 		std::unique_ptr<InstanceValue> mValue;
 	};
 
-
+	///////////////////////////////////////////////////////////////////
 	/**
 	*	\brief ParamType 参数类型
 	*/
 	template<typename T>
-	class ParamType
+	struct ParamType
 	{
 	public:
-		T& Convert(Instance& instance)
+		using ParamT = typename std::remove_cv<T>::type;
+		ParamT& Convert(Instance& instance)
 		{
-			Meta* meta = instance.GetMeta();
-			if (meta != nullptr && meta->IsSame<T>())
+			const Meta* meta = instance.GetMeta();
+			if (meta != nullptr && meta->IsSame<ParamT>())
 			{
-				return instance.GetValue<T>();
+				return instance.GetValue<ParamT>();
+			}
+		}
+	};
+
+	template<typename T>
+	struct ParamType<T*>
+	{
+	public:
+		using ParamT = typename std::remove_cv<T>::type;
+		ParamT*& Convert(Instance& instance)
+		{
+			const Meta* meta = instance.GetMeta();
+			if (meta == nullptr || !instance.IsPointer())
+			{
+				Debug::Error("Convert un-pointer instance.");
+			}
+
+			const Meta* pMeta = instance.GetPointerMeta();
+			if (pMeta != nullptr && pMeta->IsSame<ParamT>())
+			{
+				return *reinterpret_cast<ParamT**>(&instance.GetValue<void*>());
+			}
+		}
+	};
+
+	template<typename T>
+	struct ParamType<T&> : ParamType<T>, ParamType<T*>
+	{
+	public:
+		using ParamT = typename std::remove_cv<T>::type;
+		ParamT& Convert(Instance& instance)
+		{
+			if (instance.IsPointer())
+			{
+				return *ParamType<T*>::Convert(instance);
+			}
+			else
+			{
+				return ParamType<T>::Convert(instance);
 			}
 		}
 	};
@@ -323,7 +417,7 @@ namespace Refl
 	*	\brief ReturnType 返回值类型
 	*/
 	template<typename T>
-	class ReturnType
+	struct ReturnType
 	{
 	public:
 		Instance Convert(T t)
@@ -333,5 +427,5 @@ namespace Refl
 	};
 
 #include"utils\reflect.inl"
-}
+	}
 }
--- src/utils/reflect.inl
+++ src/utils/reflect.inl
@@ -1,21 +1,51 @@
-/**
-*	\brief 执行函数
-*/
-template<typename ...Args>
-inline Instance util::Meta::Call(const std::string& funcName, Args&&... args)
-{
-	Instance* instances[sizeof...(args)] = { &const_cast<Instance&>(args)... };
+#pragma once
 
+inline Instance util::Meta::Apply(Instance* args[], size_t argc, const std::string& funcName)
+{
 	auto funcInfo = mFunctions.find(funcName);
 	if (funcInfo == mFunctions.end())
 	{
 		Debug::Error("Call the non-exists meta function.");
 	}
 	FuncType funcType = funcInfo->second.funcType;
-	Instance result = funcType(instances, sizeof...(args));
+	Instance result = funcType(args, argc);
 
 	return std::move(result);
 }
+
+namespace Implemention
+{
+	inline void FuncParamWrapper(std::vector<Instance>& argVector, size_t index) {}
+
+	template<typename Head, typename... Args>
+	inline void FuncParamWrapper(std::vector<Instance>& argVector, size_t index, Head&& head, Args&&... args)
+	{
+		argVector.push_back(Instance::Make<Head>(std::forward<Head>(head)));
+		FuncParamWrapper(argVector, index + 1, std::forward<Args>(args)...);
+	}
+}
+
+template<typename ...Args>
+inline Instance util::Meta::Call(const std::string& funcName, Args&&... args)
+{
+	std::vector<Instance> mInstances;
+	std::vector<Instance*> mPointerInstance;
+	Implemention::FuncParamWrapper(mInstances, 0, std::forward<Args>(args)...);
+	for (auto& instance : mInstances)
+	{
+		mPointerInstance.push_back(&instance);
+	}
+	return Apply(mPointerInstance.data(), mInstances.size(), funcName);
+}
+
+template<typename ...Args>
+inline Instance util::Meta::CallByInstances(const std::string& funcName, Args&&... args)
+{
+	Instance* instances[sizeof...(args)] = { &const_cast<Instance&>(args)... };
+	return Apply(instances, sizeof...(args), funcName);
+}
+
+///////////////////////////////////////////////////////////////////
 
 namespace Implemention
 {
@@ -36,10 +66,12 @@ namespace Implemention
 *	\brief 记录函数的参数信息
 */
 template<typename RetType, typename... Args>
-inline void RecordFunctionArgs(RetType(*f)(Args...), Meta::FuncArgVector& argVector)
+inline void RecordFunctionArgs(std::function<RetType(Args...)> f, Meta::FuncArgVector& argVector)
 {
-	Implemention::FuncRecord(std::function<RetType(Args...)>(f), argVector);
+	Implemention::FuncRecord(f, argVector);
 }
+
+///////////////////////////////////////////////////////////////////
 
 /**
 *	\brief 函数Caller，用于对每一个instance转换成对应的参数值
@@ -52,6 +84,9 @@ inline RetType FuncCaller(Instance* args[], size_t argc, std::function<RetType()
 
 /**
 *	\brief 函数Caller，用于对每一个instance转换成对应的参数值
+*
+*	通过递归调用funcCaller,给源函数添加一层wrapper，把每个参数
+*	做一个instance转换
 */
 template<typename RetType, typename T1, typename... T>
 inline RetType FuncCaller(Instance* args[], size_t argc, std::function<RetType(T1, T...)> f)
@@ -73,13 +108,13 @@ inline RetType FuncCaller(Instance* args[], size_t argc, std::function<RetType(T
 }
 
 /**
-*	\brief 注册函数元信息
+*	\brief 注册普通函数元信息
 */
 template<typename RetType, typename ...Args>
 inline void Meta::Function(const std::string & funcName, RetType(*f)(Args...))
 {
 	Meta::FuncInfo funcInfo;
-	RecordFunctionArgs(f, funcInfo.funcArgs);
+	RecordFunctionArgs(std::function<RetType(Args...)>(f), funcInfo.funcArgs);
 
 	funcInfo.funcType = [f](Instance* args[], size_t argc)->Instance {
 		ReturnType<RetType> returnType;
@@ -94,10 +129,40 @@ template<typename ...Args>
 inline void util::Meta::Function(const std::string & funcName, void(*f)(Args...))
 {
 	Meta::FuncInfo funcInfo;
-	RecordFunctionArgs(f, funcInfo.funcArgs);
+	RecordFunctionArgs(std::function<void(Args...)>(f), funcInfo.funcArgs);
 
 	funcInfo.funcType = [f](Instance* args[], size_t argc)->Instance {
 		FuncCaller(args, argc, std::function<void(Args...)>(f));
+		return Instance();
+	};
+
+	mFunctions[funcName] = funcInfo;
+}
+
+/**
+*	\brief 注册成员通函数元信息
+*
+*	对于成员函数来讲,相比普通函数，其可变参数列表中第一个参数
+*	为类对象，std::function<RetType(T&t, Args...)>(f),当调用函数
+*	时则通过(t.*f)(args...)调用
+*/
+template<typename T, typename ReturnType, typename ...Args>
+inline void util::Meta::Function(const std::string & funcName, ReturnType(T::* f)(Args...))
+{
+}
+
+
+template<typename T, typename... Args>
+inline void util::Meta::Function(const std::string& funcName, void(T::*f)(Args...))
+{
+	Meta::FuncInfo funcInfo;
+	RecordFunctionArgs(std::function<void(T&t, Args...)>(f), funcInfo.funcArgs);
+
+	funcInfo.funcType = [f](Instance* args[], size_t argc)->Instance {
+		FuncCaller(args, argc, std::function<void(T&t, Args...)>(
+			[&args, f](T&t, Args... a) {
+			(t.*f)(std::forward<Args>(a)...);
+		}));
 		return Instance();
 	};
 
